e1645a19d88883d8f0932ce74e86f22252eaf2bb
MoazzamAliSE <102206806+MoazzamAliSE@users.noreply.github.com>
2022-07-29 03:59:23 +0500

    BottomBar and Fluent icons

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:fluent_ui/fluent_ui.dart';
 
 class BottomBar extends StatefulWidget {
   const BottomBar({Key? key}) : super(key: key);
@@ -10,29 +9,53 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    const Text("Home"),
+    const Text("Search"),
+    const Text("T"),
+    const Text("Pro"),
+  ];
+
+  void _onItemtapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    // print("${_selectedIndex}");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Ticket App")),
-      body: const Center(child: Text("Ticket Body")),
+      body: Center(child: _widgetOptions[_selectedIndex]),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemtapped,
+        elevation: 10,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        // selectedItemColor: Color.fromARGB(255, 56, 81, 122),
+        selectedItemColor: Color.fromARGB(255, 14, 50, 214),
+        unselectedItemColor: const Color(0xFF526480),
+        type: BottomNavigationBarType.fixed,
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "home",
-          ),
+              icon: Icon(FluentIcons.home),
+              activeIcon: Icon(FluentIcons.home_solid),
+              label: "home"),
           BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: "Search",
-          ),
+              icon: Icon(FluentIcons.search),
+              activeIcon: Icon(FluentIcons.search_and_apps),
+              label: "Search"),
           BottomNavigationBarItem(
-            icon: Icon(Icons.airplane_ticket),
-            label: "home",
-          ),
+              icon: Icon(FluentIcons.ticket),
+              activeIcon: Icon(FluentIcons.air_tickets),
+              label: "Ticket"),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "Search",
-          ),
+              icon: Icon(FluentIcons.people),
+              activeIcon: Icon(FluentIcons.personalize),
+              label: "Profile"),
         ],
       ),
     );
